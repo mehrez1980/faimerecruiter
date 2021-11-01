b9b9962d6c2a806a411799e0af4379cc5bf49734
MEHREZ <MEHREZ@DESKTOP-ACK9F1N>
2021-11-01 17:34:49 +0100

    Firt commit

---
+++ Test Suites/Authentification.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Authentification</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ec2f6c7c-3bc6-4bf2-a42a-505c4d2e8389</testSuiteGuid>
+   <testCaseLink>
+      <guid>5df8579e-c315-46d8-94d9-655f2b8a0371</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentification username Valide et Mdp Valide</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba48ae5e-a290-4d2c-b12e-c634c9f317a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentification username invalide</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aea5d36b-3143-4251-8d68-56279c0ca79c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Authentification password invalide</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Gestion Job order.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Gestion Job order</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>21a4ffc7-13a0-4f8f-ac44-c6b22844ce04</testSuiteGuid>
+</TestSuiteEntity>
